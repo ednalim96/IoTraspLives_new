aac442d024344355fb026aa5166bc0d4a63da6ba
ednalim <54433902+ednalim@users.noreply.github.com>
2019-12-07 17:23:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iotrasplives/HomeActivity.java
@@ -0,0 +1,89 @@
+package com.example.iotrasplives;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    TextView pirstatus, pulsebpm, camerastatus;
+    DatabaseReference dref;
+    String status, bpm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        pirstatus = findViewById(R.id.textView6);
+        pulsebpm = findViewById(R.id.textView7);
+        camerastatus = findViewById(R.id.textView8);
+        dref = FirebaseDatabase.getInstance().getReference();
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                status = dataSnapshot.child("Status").getValue().toString();
+                pirstatus.setText(status);
+
+                if(status == "No motion detected"){
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bpm = dataSnapshot.child("BPM").getValue().toString();
+                pulsebpm.setText(bpm);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                status = dataSnapshot.child("Status").getValue().toString();
+                camerastatus.setText(status);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
